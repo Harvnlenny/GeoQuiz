56fcbbed327dd4f1729f3902b9420b81d3afbf5d
Aaron Harvey <harvnlenny@gmail.com>
2016-02-10 22:38:33 -0500

    Added Lanscape Layout XML and created a saved instance state so the user can stay on the same question even as we rotate the device.

--- QuizActivity.java
+++ QuizActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 public class QuizActivity extends ActionBarActivity {
 
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -80,7 +81,18 @@ public class QuizActivity extends ActionBarActivity {
                 updateQuestion();
             }
         });
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
