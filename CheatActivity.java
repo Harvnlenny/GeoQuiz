37970d54c416310e751c1fc5cd46785b24441329
Aaron Harvey <harvnlenny@gmail.com>
2016-02-11 22:18:50 -0500

    Added new activity (cheatActivity) as a second activity.

---
+++ CheatActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class CheatActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+
+}
